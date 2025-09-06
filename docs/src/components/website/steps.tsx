e41aa38fc2b2ebdf0bc5d439d77f24b68386aeba
yefu <243249439@qq.com>
2025-09-07 01:22:40 +0800

    feat: 增加步骤条组件

---
+++ docs/src/components/website/steps.tsx
@@ -0,0 +1,13 @@
+import type { JSX } from "solid-js";
+
+export function Steps(props: { children: JSX.Element }) {
+  return <div class="relative pl-6 ml-2 border-l sm:ml-4 sm:pl-7">{props.children}</div>;
+}
+
+export function Step(props: { children: JSX.Element }) {
+  return <div>{props.children}</div>;
+}
+
+export function StepIcon(props: { children: JSX.Element }) {
+  return <span class="bg-muted text-muted-foreground absolute -start-4 size-8 rounded-full flex items-center justify-center text-sm">{props.children}</span>;
+}
